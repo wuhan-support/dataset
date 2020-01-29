ee34fd99f01b88b0c3eacfc47353907c2b84b400
Maicius <maicius@outlook.com>
2020-01-29 11:41:40 +0800

    添加定时上传任务的脚本

--- start_collect.py
+++ start_collect.py
@@ -5,18 +5,23 @@ import os
 from log_support import LogSupport
 
 def start_pull(branch_name):
-    ls.logging.info("start pull...")
-    try:
-        os.system("git add .")
-        os.system("git commit -m 'update data'")
-        os.system("git push -u origin {}".format(branch_name))
-        ls.logging.info("pull finished")
-    except BaseException as e:
-        ls.logging.exception(e)
-    time.sleep(60 * 10)
+    while True:
+        ls.logging.info("start pull...")
+        try:
+            os.system("git add .")
+            os.system("git commit -m 'update data'")
+            os.system("git push -u origin {}".format(branch_name))
+            ls.logging.info("pull finished")
+        except BaseException as e:
+            ls.logging.exception(e)
+        time.sleep(60 * 10)
 
 if __name__=='__main__':
+    """
+    使用前请在控制台切换到相应的branch: git checkout -b maiciusData
+    """
     ls = LogSupport()
     p1 = multiprocessing.Process(target=start_load_json)
     p2 = multiprocessing.Process(target=start_pull, args=['maiciusData'])
+    p1.start()
     p2.start()
